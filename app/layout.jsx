e2a8edd1c5f7c70f4721f2f1f94fc01ad696de59
thaninchote9-rgb <thaninchote9@gmail.com>
2026-05-29 14:03:35 +0300

    Add files via upload

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 export const metadata = {
@@ -42,7 +43,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
-      <body>{children}</body>
+      <body>{children}        <Analytics />
+      </body>
     </html>
   );
 }
